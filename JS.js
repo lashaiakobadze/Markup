15c1cddbf14652a501626d6b6aa6b22f1083d08c
lashaiakobadze <70689627+lashaiakobadze@users.noreply.github.com>
2020-11-28 21:15:25 +0400

    update

--- JS.js
+++ JS.js
@@ -17,7 +17,24 @@ var button = document.getElementById("start");
 var after_button = document.getElementById("start");
 var f_button = document.getElementById("subscribe");
 
+var inp1=false;
+var inp2=false;
+var inp3=false;
+var inp4=false;
+var inp5=false;
 
+var error1=false;
+var error2=false;
+var error3=false;
+var error4=false;
+var error5=false;
+
+var invalid_error1=false;
+var invalid_error2=false;
+var invalid_error3=false;
+
+var inp6=false;
+var inp7=false;
 button.addEventListener("click", function(){
     var inputName = getInput("your-name");
     var inputEmail = getInput("email");
@@ -31,66 +48,105 @@ button.addEventListener("click", function(){
     if(inputName == ""){
         updateValue_p("name-reqired", "The field is required.");
         warning_inputs("your-name");
+        error1=true;
+    }else if(error1==true){
+        document.getElementById("your-name").classList.remove("red-input");
+        document.getElementById("name-reqired").innerHTML = "";
+        inp1=true;
     }else{
-        console.log("სახელი და გვარი: "+inputName);
+        inp1=true;
     }    
 
     if(inputEmail == ""){
         updateValue_p("email-reqired", "The field is required.");
         warning_inputs("email");
+        error2=true;
     }else if(ValidateEmail(inputEmail) == false ){
         updateValue_p("email-reqired", "The e-mail address entered is invalid.");
         warning_inputs("email");
-    }else{
-        console.log("ელ.ფოსტა: "+ inputEmail);
+        invalid_error1=true;
+    }else if(error2==true || invalid_error1==true){
+        document.getElementById("email").classList.remove("red-input");
+        document.getElementById("email-reqired").innerHTML = "";
+        inp2=true;
+    }else{        
+        inp2=true;
     }
 
     if(inputNumber == ""){
         updateValue_p("phone-reqired", "The field is required.");
         warning_inputs("phone-number");
+        error3=true;
     }else if(isValidPhone(inputNumber) == false){
         updateValue_p("phone-reqired", "The telephone number is invalid.")
         warning_inputs("phone-number");
-    }else{
-        console.log("ტელ: "+inputNumber);
-    }
-
-    if(inputCompany !== ""){
-        console.log("კომპანია: "+inputCompany);
+        invalid_error2=true;
+    }else if(error3==true||invalid_error2==true){
+        document.getElementById("phone-number").classList.remove("red-input");
+        document.getElementById("phone-reqired").innerHTML = "";
+        inp3=true;
+    }else{        
+        inp3=true;
     }
 
     if(inputWebsite == ""){
         updateValue_p("website-reqired", "The field is required.");
         warning_inputs("your-website");
+        error4=true;
     }else if(is_url(inputWebsite) == false){
         updateValue_p("website-reqired", "The URL is invalid.");
         warning_inputs("your-website");
-    }else{
-        console.log("ვებ.გვერდი: "+inputWebsite);
+        invalid_error3=true;
+    }else if(error4==true||invalid_error3==true){
+        document.getElementById("your-website").classList.remove("red-input");
+        document.getElementById("website-reqired").innerHTML = "";
+        inp4=true;
+    }else{        
+        inp4=true;
     }
 
     if(inputText == ""){
         updateValue_p("project-reqired", "The field is required.");
         warning_inputs("about-project");
-    }else{
-        console.log("მოკლე ინფორმაცია: "+inputText);
-    }    
-        
-    if(after_launch !== ""){
-        console.log("დაწყების დრო: "+after_launch);
-    }
+        error5=true;
+    }else if(error5==true){
+        document.getElementById("about-project").classList.remove("red-input");
+        document.getElementById("project-reqired").innerHTML = "";
+        inp5=true;
+    }else{        
+        inp5=true;
+    }        
+   
+    if((inp5&&inp4&&inp3&&inp2&&inp1)==true){
+        console.log("სახელი და გვარი: "+inputName);
+        console.log("ელ.ფოსტა: "+ inputEmail);
+        console.log("ტელ: "+inputNumber);
 
-    if(after_budget !== ""){
-        console.log("სასურველი ბიუჯეტი: "+after_budget);
-    }
-    /**/
-    var p=this.getElementsByClassName("input");
-    for(var i=0;i<p.length;i++){
-        if(p[i].hasAttributeNS("class","red-input")==true){
-            document.getElementById("warning-box").classList.add("red-input");
+        if(inputCompany !== ""){
+            console.log("კომპანია: "+inputCompany);
         }
-    }
+
+        console.log("ვებ.გვერდი: "+inputWebsite);
+        console.log("მოკლე ინფორმაცია: "+inputText);
+
+        if(after_launch !== ""){
+            console.log("დაწყების დრო: "+after_launch);
+        }
     
+        if(after_budget !== ""){
+            console.log("სასურველი ბიუჯეტი: "+after_budget);
+        }        
+        resetForm("userForm");
+        document.getElementById("warning-box").classList.remove("erorr-form");
+
+        inp1=false;
+        inp2=false;
+        inp3=false;
+        inp4=false;
+        inp5=false;
+        }else{
+        document.getElementById("warning-box").classList.add("erorr-form");
+    }  
 });
 
 
@@ -116,6 +172,7 @@ f_button.addEventListener("click", function(){
     }
 });
 
+ //ფუნქციების შეერთება my_func = joint([ my_func, new_func ]);
 
 // ინფუთიდან ველის ამოღების ფუნქცია აიდის მიხედვით.
 function getInput(id){
@@ -140,7 +197,7 @@ function is_url(str){
     }
 }
 
-// ტელეფონის ნომრის ვალიდაცია, მაგალითისათვის ჩვენი საქართველოს ცხრა ნიშნა ნომრისათვის +995-ის გარეშე, მოთხოვნის მიხედვით შევძლებთ შესაბამისი პატერნის აწყობას ან მოძიებას.
+// ტელეფონის ნომრის ვალიდაცია, მაგალითისათვის ჩვენი საქართველოს ცხრა ნიშნა ნომრისათვის შემდეგი სახით +995 568925408 ან 568925408 მოთხოვნის მიხედვით შევძლებთ შესაბამისი პატერნის აწყობას ან მოძიებას.
 function isValidPhone(phoneNumber) {
     var found = phoneNumber.search(/^(\+{1}\d{2,3}\s?[(]{1}\d{1,3}[)]{1}\s?\d+|\+\d{2,3}\s{1}\d+|\d+){1}[\s|-]?\d+([\s|-]?\d+){1,2}$/);
     if(found > -1) {
@@ -159,6 +216,11 @@ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
     return false;
     }
 }
+
+//ფორმის დარეზეთება
+function resetForm(id){
+    document.getElementById(id).reset();
+  }
 
 // წავშალოთ დამატებითი ინფუთების შემოტანის კითხვა დაკლიკვის შემდეგ.
 function add_class_more_form() {
